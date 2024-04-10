262b93551d250fcbccf962e68bb3824a5bf6cae5
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-04-10 08:42:25 -0300

    Teste de configuração npm install moment-timezone

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,26 @@
 const { defineConfig } = require("cypress");
-const moment = require('moment');
+const moment = require('moment-timezone'); // Importa moment-timezone
 
+// Define o fuso horário para Brasília
+moment.tz.setDefault('America/Sao_Paulo');
+
+// Formata a data e hora atual para o fuso horário de Brasília
 const dateTime = moment().format('DD-MM-YYYY_HH-mm-ss');
 
 module.exports = defineConfig({
-  e2e: {
+ e2e: {
     baseUrl: 'https://www.google.com.br',
     video: true,
-  },
-  reporter: "mochawesome",
-  reporterOptions: {
+ },
+ reporter: "mochawesome",
+ reporterOptions: {
     reportDir: `cypress/reports/${dateTime}`,
     reportFilename: "[name]-report",
     quiet: true,
     overwrite: false,
     html: true,
     json: false,
-  },
-  screenshotsFolder: `cypress/reports/${dateTime}/evidências`,
-  chromeWebSecurity: false,
-  
+ },
+ screenshotsFolder: `cypress/reports/${dateTime}/evidências`,
+ chromeWebSecurity: false,
 });
